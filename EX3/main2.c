46b07a087f93240c31fc3e1c9db090601015d964
Blank0905 <109862312+Blank0905@users.noreply.github.com>
2024-12-05 15:11:21 +0800

    Create main2.c

---
+++ EX3/main2.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+int main(){
+    int i=10;
+    if(i % 3 == ) {
+        printf("Love");
+    }
+    else if (i % 5 == 0 ) {
+        printf("IU");
+    }
+    else {
+        printf("Love IU");
+    }
+return 0;
+}
