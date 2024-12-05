897fa56cb3df3a365a15df14d763c265774b303a
Blank0905 <109862312+Blank0905@users.noreply.github.com>
2024-12-05 15:21:53 +0800

    Update main4.c

--- EX3/main4.c
+++ EX3/main4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
     int i = 2000;
-    if (i%300 == 0 || (i % 4 == 0 && i % 100 != 0)) {
+    if (i%400 == 0 || (i % 4 == 0 && i % 100 != 0)) {
         printf("閏年");
     }
     else {
