e508353c8836d8e6ead3653850875ceeec1be94f
Blank0905 <109862312+Blank0905@users.noreply.github.com>
2024-12-05 16:03:46 +0800

    Update main1.c

--- HW5/main1.c
+++ HW5/main1.c
@@ -1,1 +1,10 @@
-
+#include<stdio.h>
+int main () {
+    int a = 7;
+    for(int i = 7; i > 0; i--) {
+        for(int j = 6; j>=0;j--){
+             printf(j"\n") ;  
+        }
+    }
+  return 0;
+}
