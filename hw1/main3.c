11b1a39ca6e6ecae535f5dae42b19913ed1a499d
Blank0905 <109862312+Blank0905@users.noreply.github.com>
2025-03-06 13:42:03 +0800

    Create main3.c

---
+++ hw1/main3.c
@@ -0,0 +1,35 @@
+#include<stdio.h> 
+
+int main(){
+	char a[] = "10001111";
+	int number , length , length2 ;
+	int L = 0;
+	length = sizeof(a)-1;
+	if(length%4==0) {
+		length2 = length/4;
+	} 
+	else {
+		length2 = (length/4)+1;
+	}
+
+	int sixteen [length2];
+
+	for (int i = 0 ; i<length ; i+=4) {
+		int k = 8;
+		for (int j = 0; j<4 ; j++){
+			while(1 & a [i+j]) {
+				number = number + k;
+				break;
+			}
+			k /= 2;
+		}
+		sixteen[L] = number;
+		L += 1;
+		k = 8;
+		number = 0;
+	}
+
+	for (int i = 0 ; i<length2 ; i++){
+		printf("%hx" , sixteen[i]);
+	}	
+}
